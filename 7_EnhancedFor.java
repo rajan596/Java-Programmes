574815210b06394fe75abed48bea8eaad736f354
Rajan Kasodariya <kasodariyarajan@gmail.com>
2015-05-22 16:18:11 +0530

    for ( int num : array )

---
+++ 7_EnhancedFor.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author RAJAN
+ */
+public class EnhancedFor {
+    
+    public static void main(String[] args) {
+   
+    int a[]={1,2,3,4,5,6,7,8,9};
+    
+    for(int no : a) {
+        System.out.println(no);
+    }
+
+    }
+}
