6e47251eaa01ba650c0b3c1bf89e9ccc9e7f502f
Rajan Kasodariya <kasodariyarajan@gmail.com>
2015-05-23 11:08:39 +0530

    Static Import
    We do not need to write ...Math.ceil() insteadwe can write direct ceil();

---
+++ 12_StaticImport.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author RAJAN
+ */
+
+import static java.lang.Math.*;   // static import
+
+public class StaticImport {
+    public static void main(String[] args) {
+        double a=sqrt(4);
+        double b=ceil(5.6);
+        
+        System.out.println(sqrt(a+b));
+    }
+}
