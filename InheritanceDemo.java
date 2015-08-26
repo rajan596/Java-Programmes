3d4d18b86f4b436fc6cf74da3ec0f213d98d47a5
Rajan Kasodariya <kasodariyarajan@gmail.com>
2015-08-26 16:43:30 +0530

    Inheritance  Demo

---
+++ InheritanceDemo.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author RAJAN
+ */
+public class InheritanceDemo {
+        public static void main(String args[]) {
+            Person3 p1=new Person3("xyz",19,100);
+            BusinessMan bm1=new BusinessMan("a",10,100,"x",1000);  
+            bm1.getInfo();
+            
+            
+            B b=new B();
+        }
+}
+
+class Person3 {
+    String name;
+    int age;
+    int height;
+    
+    Person3(String name , int age , int height) {
+        this.name=name;
+        this.age=age;
+        this.height=height;
+    }
+    
+    
+    public void info() {
+        System.out.println("Name : " + name + "\nAge : " + age + "\nHeight : "+height );
+    }
+}
+
+class BusinessMan extends Person3{
+    String company;
+    int salary;
+    
+    BusinessMan(String name , int age , int height , String company , int salary){
+        super(name , age , height);   /* calling parent class's constructor */
+        this.company=company;
+        this.salary=salary;
+    }
+    
+    public void getInfo() {
+        super.info();
+        System.out.println("Company :" + company+  "\nSalary : " + salary );
+    }
+}
+
+class A{
+    A() {
+        System.out.println("Class A Constructor");
+    }
+}
+
+class B extends A {
+    B() {
+        System.out.println("Class B Constructor");
+    }
+}
