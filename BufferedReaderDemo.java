3234827e2c0130914eceab4082be6c36272beb32
Rajan Kasodariya <kasodariyarajan@gmail.com>
2015-08-27 10:12:40 +0530

    BufferedReader Demo

---
+++ BufferedReaderDemo.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author RAJAN
+ */
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BufferedReaderDemo {
+        public static void main(String args[] ) throws Exception {
+
+            /*                  (Character conversion( binary flow (Input type)))
+            */                        
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String line = br.readLine();
+            int N = Integer.parseInt(line);
+            
+            System.out.println(N10
+            );
+    }
+}
