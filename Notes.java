e59693622162039fb3e7bfaefa4f15e9dbc34d90
Rajan Kasodariya <kasodariyarajan@gmail.com>
2016-01-02 11:08:41 +0530

    Update Notes.java

--- Notes.java
+++ Notes.java
@@ -15,8 +15,30 @@
     >> StringBuilder : 
     -> It is not synchronizes so It can't be used in Threads. StringBuffer is better for this.
 
-4. int defaultVariable              //  Visible and accesed inside same package 
+4. int defaultVariable              //  Visible and accesed to same package 
    public int publicVariable        //  Visible to all packages
    private int privateVariable      //  Visible to only class in which it is declared 
-   protected int protectedVariable  //  Visible to same and inherited classes. 
+   protected int protectedVariable  //  Visible to same package and inherited classes from different pakages. 
    
+5. final variable  :- Can't change the value
+   final method    :- can't @override method
+   final class     :- can't extend this class
+   
+6. overriding Vs Overloading :
+   class A {
+       public void funct(int x) {
+           
+       }
+       public void funct(String s) {  // This is overloading
+           
+       }
+   }
+   
+   class B extends A {
+       @override
+       public void funct(int x) {   // This is overriding
+           
+       }
+   }
+
+7. 
